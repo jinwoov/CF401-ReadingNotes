51e226ef001c70baa4490a8650892c492dd2e346
Jin <jinwoov@gmail.com>
2020-03-19 01:09:22 -0700

    finished my readme for reading 04

---
+++ Class-Example/SystemIO/SystemIO/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+
+namespace SystemIODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //WriteToFile();
+            //ReadaFile();
+            //FileAppendText();
+
+            //ReadAllLines();
+            DeleteAFile();
+        }
+        // Create a File
+        static void WriteToFile()
+        {
+            // Defined the file 
+            // change the path to specific location
+            string path = "../../../jin.txt";
+            // Wrote to the file specific text
+            File.WriteAllText(path, "Coronavirus popclyse");
+
+        }
+
+        // Read a file
+        static void ReadaFile()
+        {
+            string path = "../../../jin.txt";
+            string myFile = File.ReadAllText(path);
+            Console.WriteLine(myFile);
+        }
+
+        static void ReadAllLines()
+        {
+            string path = "../../../jin.txt";
+            string[] line = File.ReadAllLines(path);
+            Console.WriteLine(String.Join(", ", line));
+        }
+
+        // adding text
+        static void FileAppendText()
+        {
+            string[] words = { "", "The cat in the hat", "beets", "bear", "battlegalactica" };
+            string path = "../../../jin.txt";
+
+            File.AppendAllLines(path, words);
+        }
+        //alternative way to read a file
+        static void ReadFile()
+        {
+            string path = "../../../jin.txt";
+
+            try
+            {
+                // using a stream/ opening a connection
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string text = "";
+                    while ((text = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Your file is not available");
+            }
+            finally
+            {
+                Console.WriteLine("The process is completed");
+            }
+        }
+        // Update file
+
+        //static void UpdateFile()
+        //{
+        //    string path = "../../../jin.txt";
+
+        //    // Add the numbers 0 - 4 in the text
+        //    try
+        //    {
+        //        using (StringWriter sr = File.AppendText(path))
+        //        {
+        //            for (int i = 0; i < 5; i++)
+        //            {
+        //                sr.WriteLine(i);
+        //            }
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+        //}
+
+
+        // Delete a file
+
+        static void DeleteAFile()
+        {
+            string potato = "../../../jin.txt";
+
+            File.Delete(potato);
+        }
+
+        static void PracticeUsingSplit()
+        {
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            string text = "one\ttwo three:four, five six seven";
+
+            string[] words = text.Split(delimiterChars);
+
+            foreach (string item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ In-Class-Notes/day3.md
@@ -0,0 +1,71 @@
+- Dont do the throw if you want program to continue
+- 
+
+File manipulation
+- when modify and edit a file
+- create a file
+- deleting a file
+- form oof data persistence
+
+Read from csv file
+- parse and manipulate it
+
+SystemIO
+- I: input
+- O: output
+- libary
+bin/debug/app/text
+
+- stream is like channel or pipeline of bytes
+    - like river
+
+- water a to b
+- stream is an object
+- has method properties and variables
+
+- streams are in one direction
+    - its either input or output
+
+- stream to input data and to read the data
+
+
+file stream you can use stream reader or streamwriter.
+
+CRUD
+
+0(1) - constant x
+0(log n) logrithic time /dividing by constant factorx  y
+o(n)- linearx
+o(n^2)-nested for loop
+o(2n)- information that comes in 2 is constant
+o(n3)
+o(n logn)-sorting
+o(n!) - 5,4,3,2,1
+    - permutation, time 
+    - con
+space
+    - [1,2,3,4,5,6] L=6 n
+    - log n
+    worst case scenario
+
+omega - best - don't worry to much stee slop
+theta 0 - average - efficiency randomly picking one of the iteration
+    - random iteration 
+- 
+big o - worst
+        - rate of growth
+        - trillions
+        - 
+
+
+space
+- memory
+- method A bring in array
+- On - O  
+- O1 - 
+- space is still growing
+- O(1)
+- Oh - number of frame
+- On^2 
+
+Recurssion binary sorting algorithm
